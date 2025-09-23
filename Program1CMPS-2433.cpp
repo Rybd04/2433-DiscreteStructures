a5e58809f18ea902c8ab0e76929f931985b0d326
Rybd04 <9407821129rd@gmail.com>
2025-09-23 18:59:50 +0000

    Done

--- Program1CMPS-2433.cpp
+++ Program1CMPS-2433.cpp
@@ -1,38 +1,30 @@
+// Ryan Darden 
+// CMPS 2433 - Stringfellow
+// Programming Assignment 1
+// 9/24/25
+
 #include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
 
-void openFiles(ifstream& infile, ofstream& outfile)
-{
-	string inFileName;
-	string outFileName;
-	// cout<<"Enter the input file name: ";
-	// cin>>inFileName;
-	inFileName = "SampleInput.txt";
-	infile.open(inFileName); // open input file
-	// cout<<"Enter the output file name: ";
-	// cin>> outFileName;
-	outFileName = "SampleOutput.txt";
-	outfile.open(outFileName);//open out putfile
-}
-
-int computeGrayInt(int n, int k) {
-	int position = k ^ (k >> 1);
-	cout << position;
-	cout << endl;
-	return position;
-}
+// Purpose: Opens files
+// Requires: Access to input and output files and streams
+// Returns: n/a
+void openFiles(ifstream& infile, ofstream& outfile);
 
 
-int computeGrayPosition(int g) {
-	int k = 0;
+// Purpose: to compute & return the integer corresponding to
+// the gray number that appears in position k of the n-bit Gray Code using bit manipulation.
+// Requires: Position and number of bits
+// Returns: the gray code integer
+int computeGrayInt(int n, int k);
 
-	for (; g > 0; g >>= 1) {
-		k ^= g;
-	}
-	return k;
-}
+// Purpose: that when given a gray code, computes its
+// position k in the n-bit Gray Code.
+// Requires: Gray code
+// Returns: The position of the gray code
+int computeGrayPosition(int g);
 
 int main()
 {
@@ -43,26 +35,56 @@ int main()
 	char type;
 	int n, k;
 	int count = 0;
+	
+	infile >> count;	//Get count of test cases
 
-	outfile << "Ryan Darden";
+	outfile << "Ryan Darden" << endl;
 
-	infile >> count;
-
-	for (int i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++) {	//Loop for each test case
 		infile >> type;
-		if (type == 'A') {
+		if (type == 'A') {		//Type A: Find gray code at position k
 			infile >> n >> k;
 			int gray = computeGrayInt(n, k);
+			outfile << gray << endl;
 		}
-		else if (type == 'B') {
+		else if (type == 'B') {		//Type B: Find position of gray code g
 			int g = 0;
 			infile >> g;
 			int pos = computeGrayPosition(g);
-			cout << pos << endl;
+			outfile << pos << endl;
 		}
-		else {
+		else {		//Invalid type
 			outfile << "Invalid Type";
 			break;
 		}
 	}
+}
+
+
+void openFiles(ifstream& infile, ofstream& outfile)
+{
+	string inFileName;
+	string outFileName;
+	cout<<"Enter the input file name: ";
+	cin>>inFileName;
+	infile.open(inFileName); // open input file
+	cout<<"Enter the output file name: ";
+	cin>> outFileName;
+	outfile.open(outFileName);//open out putfile
+}
+
+
+int computeGrayInt(int n, int k) {
+	int gray = k ^ (k >> 1); 		// Formula for computing gray code from binary number
+	return gray;
+}
+
+int computeGrayPosition(int g) {
+	int k = 0;
+
+	for (; g > 0; g >>= 1) {		// Loop until g is 0, shifting g right each iteration
+		k ^= g;
+	}
+	cout << k;
+	return k;
 }
